2204c5f23f3e8ca6a48a09273f101457977cfd02
21Cash <21ThousandProof@gmail.com>
2025-12-10 19:28:40 +0530

    Init React Typescript TailwindCSS

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="bg-gray-900 min-h-screen">
+      <div className="text-6xl font-thin flex items-center justify-center h-screen text-white">
+        React TypeScript TailwindCSS Kickoff Repo
+      </div>
+    </div>
+  );
+};
+
+export default App;
